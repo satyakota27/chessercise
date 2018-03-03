43cab4103ad64987947737a616d6b598cac7d0c5
ksnmurthy <ksnmurthy4564@gmail.com>
2018-03-03 17:28:44 -0500

    Location coordinates are updated

--- chessercise.rb
+++ chessercise.rb
@@ -4,11 +4,29 @@ unless ARGV.length == 4
   exit
 end
 
+rook_moves = [{ x: 1, y: 0, steps: 7},
+              { x: 0, y: 1, steps: 7},
+              { x: 0, y: -1, steps: 7},
+              { x: -1, y: 0, steps: 7}]
 
 piece_type = ARGV[1].to_s
-location = ARGV[3].to_s
+location = ARGV[3].downcase
 puts piece_type, location
 
+unless ['KNIGHT','ROOK','QUEEN'].include?(piece_type.upcase)
+  puts 'please enter a valid piece type. KNIGHT or ROOK or QUEEN'
+  exit
+end
+
+#setting up location coordinates
+location_coords = location.split(//)
+x = location_coords[0].ord - 96
+y = location_coords[1]
+
+if x < 1 || y < 1 || y > 8 || x > 8 || location.length!=2
+  puts 'please provide a valid location for the piece'
+  exit
+end
 
 def calculate_possible_moves(piece, location)
   'h1 h2' #sample output
